f719b33aa9ccc7641e4ec0192c4ff552c6ae0c46
vcmog <scvcm@leeds.ac.uk>
2025-12-09 15:30:15 +0000

    Correct errors for linting

--- poisson_cost.py
+++ poisson_cost.py
@@ -1,5 +1,40 @@
-import numpy as np
+"""
+Docstring for poisson_cost
+Poisson Change Point Detection Module.
+This module provides functionality for detecting change points in Poisson-distributed
+count data using the PELT (Pruned Exact Linear Time) algorithm with a custom Poisson
+cost function.
+The implementation includes:
+- CostPoisson: A custom cost function class for Poisson-distributed data based on
+    negative log-likelihood, compatible with the ruptures library.
+- detect_poisson_change_points: A function to detect change points in count data
+    using the PELT algorithm.
+- plot_change_points: A visualization function to plot count data with detected
+    change points.
+The cost function implementation follows the approach described in:
+Truong et al. (2019), "Selective review of offline change point detection methods",
+Signal Processing, doi: 10.1016/j.sigpro.2019.107299
+Example
+>>> import numpy as np
+>>> from poisson_cost import detect_poisson_change_points, plot_change_points
+>>>
+>>> # Generate sample Poisson data with change points
+>>> time_points = np.arange(100)
+>>> count_values = np.concatenate([
+...     np.random.poisson(5, 30),
+...     np.random.poisson(15, 40),
+...     np.random.poisson(8, 30)
+... ])
+>>>
+>>> # Detect change points
+>>> change_points = detect_poisson_change_points(count_values, time_points, penalty=1)
+>>>
+>>> # Visualize results
+>>> plot_change_points(time_points, count_values, change_points)
+"""
+
 from math import log
+import numpy as np
 import matplotlib.pyplot as plt
 import ruptures as rpt
 from ruptures.base import BaseCost
@@ -14,18 +49,22 @@ class CostPoisson(BaseCost):
     Cost function is taken from Truong et. al 2019, doi: 10.1016/j.sigpro.2019.107299.
     """
 
-    model = ""
+    model = "Poisson"
     min_size = 1
 
-    def fit(self, signal):
+    def __init__(self):
+        super().__init__()
+        self.signal = None
+
+    def fit(self, *args, **kwargs):
         """Fit the cost function to the signal.
 
         Parameters
         ----------
-        signal : 1D array-like, shape (n_samples,)
-            The input signal to fit the cost function to.
+        *args : tuple
+            The first element is expected to be the signal (1D or 2D array-like).
         """
-        self.signal = np.asarray(signal)
+        self.signal = np.asarray(args[0])
         return self
 
     def error(self, start, end):
@@ -51,7 +90,8 @@ class CostPoisson(BaseCost):
         # Avoid log(0) by replacing zeros with a small value
         mean_segment = np.where(mean_segment == 0, 1e-10, mean_segment)
 
-        # Compute the negative log-likelihood for Poisson distribution (simplified to remove additive constants)
+        # Compute the negative log-likelihood for Poisson distribution
+        # (simplified to remove additive constants)
         cost = -(end - start) * (mean_segment * log(mean_segment))
         return cost
 
